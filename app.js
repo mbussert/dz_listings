f2377da6f20632203d4616dc4403f210e93619d9
bacloud14 <bacloud14@gmail.com>
2021-04-13 02:32:39 +0200

    anti spam anti bad words

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var _ = require('underscore');
 var db = require('./helper').db
 const dotenv = require('dotenv')
+var Filter = require('bad-words');
 
 var indexRouter = require('./routes/index');
 var listingsRouter = require('./routes/listings');
@@ -24,10 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+var customFilter = new Filter({ placeHolder: 'x'});
 var trimmer = function(req, res, next){
   req.body = _.object(_.map(req.body, function (value, key) {
-    return [key, value.trim()];
+    return [key, customFilter.clean(value.trim())];
   }));
   next();
 }
@@ -56,5 +57,23 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+const dns = require('dns')
+app.use(function (req, res, next) {
+  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  if (ip.substr(0, 7) === '::ffff:') {
+    ip = ip.substr(7)
+  }
+
+  if (process.env.NODE_ENV === 'dev' || ip.split('.')[0] === '127') { return next() }
+  const reversedIp = ip.split('.').reverse().join('.')
+  dns.resolve4([process.env.HONEYPOT_KEY, reversedIp, 'dnsbl.httpbl.org'].join('.'), function (err, addresses) {
+    if (!addresses) { return next() }
+    const _response = addresses.toString().split('.').map(Number)
+    const test = (_response[0] === 127 && _response[3] > 0) // visitor_type[_response[3]]
+    if (test) { res.send({ msg: 'we hate spam to begin with!' }) }
+    return next()
+  })
+})
 
 module.exports = app;
